fdd29860d1ee394325296d03c096bec3b73327f7
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-10-12 21:21:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+console.log(__dirname)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html', {options: {'Feature-Policy': "fullscreen 'self'"}})
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(__dirname + '/style.css')
+});
+
+app.get('/goose.jpeg', (req, res) => {
+  res.sendFile(__dirname + '/goose.jpeg')
+});
+
+app.get('/script.js', (req, res) => {
+  res.sendFile(__dirname + '/script.js')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
---
+++ script.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById('example');
+const ctx = canvas.getContext('2d');
+var goose = document.querySelector('canvas#goose');
+let timerstart = Date.now()
+let vars = [0, 0, 16, 10]
+let keys = {a:false,d:false,w:false,s:false}
+function drawGoose(options) {
+  let ref = document.querySelector('img#goose');
+  
+  let ocanvas = document.querySelector('canvas#goose');
+  ocanvas.width = ref.naturalWidth/10;
+  ocanvas.height = ref.naturalHeight/10;
+  let octx = ocanvas.getContext('2d');
+  octx.clearRect(0, 0, ocanvas.width, ocanvas.height);
+  if (options != undefined) {
+    if (options.flip == true) {
+      octx.scale(-1,1);
+    }
+  }
+  octx.drawImage(ref, 0, 0);
+  return;
+}
+function timer() {
+  return (Date.now() - timerstart) / 1000
+}
+ctx.fillStyle = "white"
+drawGoose()
+function render() {
+  vars[0]+= (keys.d - keys.a) * 5;
+  vars[1]+= vars[3]+(keys.s - keys.w) * 5;
+  vars[3]+=0.1
+  if (vars[0] > 960) { vars[2] = -3}
+  if (vars[0] < 0) {vars[2] = 3}
+  if (vars[1] > 600) {vars[3] = Math.random() * -15}
+  if (vars[1] < 0) {vars[3] = 4}
+  goose = document.querySelector('canvas#goose');
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(goose, vars[0], vars[1]);
+}
+setInterval(render, 8.35)
+document.addEventListener("keydown", (e)=>{
+  keys[e.key] = true
+});
+document.addEventListener("keyup", (e)=>{
+  keys[e.key] = false
+});
